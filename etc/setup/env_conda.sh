cf03d7eedba7eb3df295e260a34a2744114d7867
ga84jog <ga84jog@tum.de>
2024-09-17 14:53:44 +0200

    removed sudo command from env_downloads (fails on remote)

--- etc/setup/env_conda.sh
+++ etc/setup/env_conda.sh
@@ -38,7 +38,7 @@ echo -e "\033[34m[6/6]\033[0m Activating mimic3 conda environment"
 conda activate mimic3
 
 export CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.2/lib64   # $LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
 
 python -m dotenv -f ${WORKINGDIR}/.env set CUDNN_PATH ${CUDNN_PATH}
 python -m dotenv -f ${WORKINGDIR}/.env set LD_LIBRARY_PATH ${LD_LIBRARY_PATH}
--- etc/setup/env_downloads.sh
+++ etc/setup/env_downloads.sh
@@ -17,20 +17,20 @@ echo -e "\033[34m[3/5]\033[0m Creating benchmark download dir at $CONFIG/mimic3-
 # Fetch settings files from original mimic3 directory
 destinationDir="$CONFIG/mimic3-benchmark"
 if [ ! -d "$destinationDir" ]; then
-    sudo mkdir -p "$destinationDir"
+    mkdir -p "$destinationDir"
 fi
 outputVariableMap="$destinationDir/itemid_to_variable_map.csv"
 outputDefinitions="$destinationDir/hcup_ccs_2015_definitions.yaml"
 
 if [ ! -f "$outputVariableMap" ]; then
     echo -e "\033[34m[4/5]\033[0m Downloading itemid_to_variable_map.csv"
-    sudo wget "https://raw.githubusercontent.com/YerevaNN/mimic3-benchmarks/master/mimic3benchmark/resources/itemid_to_variable_map.csv" -O "$outputVariableMap"
+    wget "https://raw.githubusercontent.com/YerevaNN/mimic3-benchmarks/master/mimic3benchmark/resources/itemid_to_variable_map.csv" -O "$outputVariableMap"
 else
     echo -e "\033[34m[4/5]\033[0m itemid_to_variable_map.csv already exists"
 fi
 if [ ! -f "$outputDefinitions" ]; then
     echo -e "\033[34m[5/5]\033[0m Downloading hcup_ccs_2015_definitions.yaml"
-    sudo wget "https://raw.githubusercontent.com/YerevaNN/mimic3-benchmarks/master/mimic3benchmark/resources/hcup_ccs_2015_definitions.yaml" -O "$outputDefinitions"
+    wget "https://raw.githubusercontent.com/YerevaNN/mimic3-benchmarks/master/mimic3benchmark/resources/hcup_ccs_2015_definitions.yaml" -O "$outputDefinitions"
 else
     echo -e "\033[34m[5/5]\033[0m hcup_ccs_2015_definitions.yaml already exists"
 fi
