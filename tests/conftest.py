e46c792a62b75335b9a1ba76d4a25bed9f6bea96
ga84jog <ga84jog@tum.de>
2024-10-09 19:57:57 +0000

    Introduced env vars into workflow and improve junitxml outputs

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,20 +40,20 @@ def pytest_configure(config) -> None:
     test_file = config.getoption("file_or_dir")
 
     # Junit
-    if test_file:
-        # Extract the base name of the first test file for simplicity
-        base_name = str(Path(test_file[0])\
-            .relative_to("tests"))\
-            .strip(".py")\
-            .replace("/", ".")\
-            .replace("\\", ".")
-
-        result_dir = Path(os.getenv("TESTS"), "data", "pytest-results")
-        result_dir.mkdir(parents=True, exist_ok=True)
-        junit_path = Path(result_dir, f"{base_name}_junit.xml")
-
-        # Set the JUnit report name based on the test file name
-        config.option.xmlpath = junit_path
+    # if test_file:
+    #     # Extract the base name of the first test file for simplicity
+    #     base_name = str(Path(test_file[0])\
+    #         .relative_to("tests"))\
+    #         .strip(".py")\
+    #         .replace("/", ".")\
+    #         .replace("\\", ".")
+    #
+    #     result_dir = Path(os.getenv("TESTS"), "data", "pytest-results")
+    #     result_dir.mkdir(parents=True, exist_ok=True)
+    #     junit_path = Path(result_dir, f"{base_name}_junit.xml")
+    #
+    #     # Set the JUnit report name based on the test file name
+    #     config.option.xmlpath = junit_path
 
 
 @pytest.fixture(scope="function", autouse=True)
