ad299dc40776841317e4242b209ad8f84d11eed4
ga84jog <ga84jog@tum.de>
2024-10-11 13:44:54 +0000

    Expanding colored outputs

--- .github/scripts/run_container.sh
+++ .github/scripts/run_container.sh
@@ -7,25 +7,30 @@ COMMAND="${3}"
 BASH_RESULTS="${4}"
 OUTPUT_FILENAME="${5}"
 
-BLUE="\033[0;34m"
-RESET="\033[0m"
+RED='\033[31;1m'
+BLUE='\033[34;1m'
+LIGHT_BLUE='\033[94m'
+GREEN='\033[32;1m'
+RESET='\033[0m'
 
 echo -e ""
 echo -e "${BLUE}=========== .github/scripts/run_container.sh ================"
-echo -e "${BLUE}- Docker volume: ${RESET}$DOCKER_VOLUME_MOUNTS"
-echo -e "${BLUE}- Branch name: ${RESET}$BRANCH_NAME"
-echo -e "${BLUE}- Command: ${RESET}$COMMAND"
-echo -e "${BLUE}- Bash results: ${RESET}$BASH_RESULTS"
-echo -e "${BLUE}- Output filename: ${RESET}$OUTPUT_FILENAME"
+echo -e "${BLUE}- Docker volume: ${LIGHT_BLUE}$DOCKER_VOLUME_MOUNTS"
+echo -e "${BLUE}- Branch name: ${LIGHT_BLUE}$BRANCH_NAME"
+echo -e "${BLUE}- Command: ${LIGHT_BLUE}$COMMAND"
+echo -e "${BLUE}- Bash results: ${LIGHT_BLUE}$BASH_RESULTS"
+echo -e "${BLUE}- Output filename: ${LIGHT_BLUE}$OUTPUT_FILENAME${RESET}"
 echo -e ""
 
 set -o pipefail
 
+echo "::group::Container command"
 echo -e "${BLUE}Running command:${REST}\n \
     docker run $DOCKER_VOLUME_MOUNTS \
     tensorpod/pymimic3:$BRANCH_NAME \
-    bash -ic "$COMMAND 2>&1" \
+    bash -ic \"$COMMAND 2>&1\" \
     | tee $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}\n"
+echo "::endgroup::"
 
 # Running the pytest command inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
@@ -37,14 +42,15 @@ docker run $DOCKER_VOLUME_MOUNTS \
 test_status=$?
 
 # Printing and handling the exit status
-echo -e "${BLUE}---------- Exit status: $test_status--------------------------------${RESET}"
-echo -e "Log artifact located at: \
-    $BASH_RESULTS/$OUTPUT_FILENAME.txt"
+echo -e "${BLUE}---------- Exit status: $test_status --------------------------------"
+echo -e "${BLUE}Log artifact located at:${LIGHT_BLUE} \
+    $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
 if [ $test_status -ne 0 ]; then
-    echo "The command failed."
+    echo "${RED}The command failed.${RESET}"
     exit $test_status
 elif [ ! -f "$BASH_RESULTS/$OUTPUT_FILENAME.txt" ]; then
-    echo "Log artifact not created."
+    echo "${RED}Log artifact not created. Expected location:\n \
+        $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
     exit $(( $test_status == 0 ? 1 : $test_status ))
 fi
 echo ""
--- .github/scripts/run_pytest_container.sh
+++ .github/scripts/run_pytest_container.sh
@@ -9,25 +9,33 @@ BASH_RESULTS="${5}"
 PYTEST_RESULTS="${6}"
 OUTPUT_FILENAME="${7}"
 
+RED='\033[31;1m'
+BLUE='\033[34;1m'
+LIGHT_BLUE='\033[94m'
+GREEN='\033[32;1m'
+RESET='\033[0m'
+
 echo ""
 echo "${BLUE}=========== .github/scripts/run_container.sh ================"
-echo "${BLUE}- Docker volume: ${RESET}$DOCKER_VOLUME_MOUNTS"
-echo "${BLUE}- Branch name: ${RESET}$BRANCH_NAME"
-echo "${BLUE}- Container pytest results: ${RESET}$CONTAIER_PYTEST_RESULTS"
-echo "${BLUE}- Pytest module path: ${RESET}$PYTEST_MODULE_PATH"
-echo "${BLUE}- Bash results: ${RESET}$BASH_RESULTS"
-echo "${BLUE}- Pytest results: ${RESET}$PYTEST_RESULTS"
-echo "${BLUE}- Output filename: ${RESET}$OUTPUT_FILENAME"
+echo "${BLUE}- Docker volume: ${LIGHT_BLUE}$DOCKER_VOLUME_MOUNTS"
+echo "${BLUE}- Branch name: ${LIGHT_BLUE}$BRANCH_NAME"
+echo "${BLUE}- Container pytest results: ${LIGHT_BLUE}$CONTAIER_PYTEST_RESULTS"
+echo "${BLUE}- Pytest module path: ${LIGHT_BLUE}$PYTEST_MODULE_PATH"
+echo "${BLUE}- Bash results: ${LIGHT_BLUE}$BASH_RESULTS"
+echo "${BLUE}- Pytest results: ${LIGHT_BLUE}$PYTEST_RESULTS"
+echo "${BLUE}- Output filename: ${LIGHT_BLUE}$OUTPUT_FILENAME${RESET}"
 echo ""
 
 set -o pipefail
 
+echo "::group::Pytest container command"
 echo -e "${BLUE}Running command:${REST}\n \
     docker run $DOCKER_VOLUME_MOUNTS \
     tensorpod/pymimic3:$BRANCH_NAME \
-    bash -ic "pytest --no-cleanup --junitxml=$CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
-    -v $PYTEST_MODULE_PATH 2>&1 && $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.html" \
+    bash -ic \"pytest --no-cleanup --junitxml=$CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
+    -v $PYTEST_MODULE_PATH 2>&1 && $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.html\" \
     | tee $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}\n"
+echo "::endgroup::"
 
 # Running the pytest command inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
@@ -39,10 +47,12 @@ docker run $DOCKER_VOLUME_MOUNTS \
 # Capture the exit status of the pytest command
 test_status=$?
 
+echo "::group::Pytest summary command"
 echo -e "${BLUE}docker run $DOCKER_VOLUME_MOUNTS \
     tensorpod/pymimic3:$BRANCH_NAME \
-    bash -ic "python -m tests.pytest_utils.reporting $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
-    2>&1" | tee -a $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}\n"
+    bash -ic \"python -m tests.pytest_utils.reporting $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
+    2>&1\" | tee -a $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}\n"
+echo "::endgroup::"
 
 # Run the reporting tool inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
@@ -52,17 +62,19 @@ docker run $DOCKER_VOLUME_MOUNTS \
 
 # Printing and handling the exit status
 echo -e "${BLUE}---------- Exit status: $test_status--------------------------------${RESET}"
-echo -e "${BLUE}Log artifact located at:\n$BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
-echo -e "${BLUE}Pytest artfact created at:\n$PYTEST_RESULTS/$OUTPUT_FILENAME.xml${RESET}"
-echo -e "${BLUE}Pytest artfact created at:\n$PYTEST_RESULTS/$OUTPUT_FILENAME.xml${RESET}"
+echo -e "${BLUE}Log artifact located at:\n${LIGHT_BLUE}$BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
+echo -e "${BLUE}Pytest artfact created at:\n${LIGHT_BLUE}$PYTEST_RESULTS/$OUTPUT_FILENAME.xml${RESET}"
+echo -e "${BLUE}Pytest artfact created at:\n${LIGHT_BLUE}$PYTEST_RESULTS/$OUTPUT_FILENAME.xml${RESET}"
 
 if [ $test_status -ne 0 ]; then
-    echo "The command failed."
+    echo "${BLUE}The command failed.${RESET}"
     exit $test_status
 elif [ ! -f "$BASH_RESULTS/$OUTPUT_FILENAME.txt" ]; then
-    echo "Log artifact not created."
+    echo "${RED}Bash artifact not created. Expected location:\n \
+        $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
     exit $(( $test_status == 0 ? 1 : $test_status ))
 elif [ ! -f "$PYTEST_RESULTS/$OUTPUT_FILENAME.xml" ]; then
-    echo "Pytest artifact not created."
+    echo "${RED}Pytest artifact not created. Expected location:\n \
+        $BASH_RESULTS/$OUTPUT_FILENAME.txt${RESET}"
     exit $(( $test_status == 0 ? 1 : $test_status ))
 fi
