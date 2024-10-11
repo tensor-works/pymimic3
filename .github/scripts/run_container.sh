37bbb59f668d13c57fd076815859bbf9ea3d8a60
ga84jog <ga84jog@tum.de>
2024-10-11 11:05:42 +0000

    Fixed run pytest container .sh

--- .github/scripts/run_container.sh
+++ .github/scripts/run_container.sh
@@ -4,16 +4,17 @@
 DOCKER_VOLUME_MOUNTS="${1}"
 BRANCH_NAME="${2}"
 COMMAND="${3}"
-CONTAINER_BASH_RESULTS="${4}"
-BASH_RESULTS="${5}"
-OUTPUT_FILENAME="${6}"
+BASH_RESULTS="${4}"
+OUTPUT_FILENAME="${5}"
 
-echo "Docker volume: $DOCKER_VOLUME_MOUNTS"
-echo "Branch name: $BRANCH_NAME"
-echo "Command: $COMMAND"
-echo "Contianer bash results: $CONTAINER_BASH_RESULTS"
-echo "Bash results: $BASH_RESULTS"
-echo "Output filename: $OUTPUT_FILENAME"
+echo ""
+echo "=========== .github/scripts/run_container.sh ================"
+echo "- Docker volume: $DOCKER_VOLUME_MOUNTS"
+echo "- Branch name: $BRANCH_NAME"
+echo "- Command: $COMMAND"
+echo "- Bash results: $BASH_RESULTS"
+echo "- Output filename: $OUTPUT_FILENAME"
+echo ""
 
 # Running the pytest command inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
@@ -25,7 +26,7 @@ docker run $DOCKER_VOLUME_MOUNTS \
 test_status=$?
 
 # Printing and handling the exit status
-echo "Exit status: $test_status"
+echo "---------- Exit status: $test_status--------------------------------"
 if [ $test_status -ne 0 ]; then
     echo "The command failed."
     exit $test_status
@@ -33,3 +34,4 @@ elif [ ! -f "$BASH_RESULTS/$OUTPUT_FILENAME.txt" ]; then
     echo "Log artifact not created."
     exit $(( $test_status == 0 ? 1 : $test_status ))
 fi
+echo ""
--- .github/scripts/run_pytest_container.sh
+++ .github/scripts/run_pytest_container.sh
@@ -4,18 +4,28 @@
 DOCKER_VOLUME_MOUNTS="${1}"
 BRANCH_NAME="${2}"
 CONTAINER_PYTEST_RESULTS="${3}"
-CONTAINER_BASH_RESULTS="${4}"
+PYTEST_MODULE_PATH="${4}"
 BASH_RESULTS="${5}"
 PYTEST_RESULTS="${6}"
-PYTEST_MODULE_PATH="${7}"
-OUTPUT_FILENAME="${8}"
+OUTPUT_FILENAME="${7}"
+
+echo ""
+echo "=========== .github/scripts/run_container.sh ================"
+echo "- Docker volume: $DOCKER_VOLUME_MOUNTS"
+echo "- Branch name: $BRANCH_NAME"
+echo "- Container pytest results: $CONTAIER_PYTEST_RESULTS"
+echo "- Pytest module path: $PYTEST_MODULE_PATH"
+echo "- Bash results: $BASH_RESULTS"
+echo "- Pytest results: $PYTEST_RESULTS"
+echo "- Output filename: $OUTPUT_FILENAME"
+echo ""
 
 # Running the pytest command inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
     tensorpod/pymimic3:$BRANCH_NAME \
-    bash -ic "pytest --no-cleanup --junitxml=$CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME \
-    -v $PYTEST_MODULE_PATH 2>&1 \
-    | tee $CONTAINER_BASH_RESULTS/${OUTPUT_FILENAME%.xml}.txt"
+    bash -ic "pytest --no-cleanup --junitxml=$CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
+    -v $PYTEST_MODULE_PATH 2>&1" \
+    | tee $BASH_RESULTS/$OUTPUT_FILENAME.txt
 
 # Capture the exit status of the pytest command
 test_status=$?
@@ -23,18 +33,18 @@ test_status=$?
 # Run the reporting tool inside a Docker container
 docker run $DOCKER_VOLUME_MOUNTS \
     tensorpod/pymimic3:$BRANCH_NAME \
-    bash -ic "python -m tests.pytest_utils.reporting $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME \
-    2>&1 | tee -a $CONTAINER_BASH_RESULTS/${OUTPUT_FILENAME%.xml}.txt"
+    bash -ic "python -m tests.pytest_utils.reporting $CONTAINER_PYTEST_RESULTS/$OUTPUT_FILENAME.xml \
+    2>&1" | tee -a $BASH_RESULTS/$OUTPUT_FILENAME.txt
 
 # Printing and handling the exit status
 echo "Exit status: $test_status"
 if [ $test_status -ne 0 ]; then
     echo "The command failed."
     exit $test_status
-elif [ ! -f "$BASH_RESULTS/${OUTPUT_FILENAME%.xml}.txt" ]; then
+elif [ ! -f "$BASH_RESULTS/$OUTPUT_FILENAME.txt" ]; then
     echo "Log artifact not created."
     exit $(( $test_status == 0 ? 1 : $test_status ))
-elif [ ! -f "$PYTEST_RESULTS/$OUTPUT_FILENAME" ]; then
+elif [ ! -f "$PYTEST_RESULTS/$OUTPUT_FILENAME.xml" ]; then
     echo "Pytest artifact not created."
     exit $(( $test_status == 0 ? 1 : $test_status ))
 fi
